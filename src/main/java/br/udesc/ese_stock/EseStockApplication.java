96453036275498a34b8edb9b5220547d71138723
Daniel Larion Klug <dklug22@gmail.com>
2024-09-16 22:39:24 -0300

    init docker

---
+++ src/main/java/br/udesc/ese_stock/EseStockApplication.java
@@ -0,0 +1,13 @@
+package br.udesc.ese_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EseStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EseStockApplication.class, args);
+	}
+
+}
